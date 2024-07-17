30cf31a5e9f03ff517897c3bd43d67f3ffdfe099
John Patrick Pahunang <patrickpahunang@Patricks-MacBook-Air.local>
2024-07-17 09:01:43 +0800

    tested contents nalang ang kulang

--- src/App.tsx
+++ src/App.tsx
@@ -24,18 +24,18 @@ const App = () => {
 
   return (
     <Router>
-      <div className="font-outfit bg-zinc-900 min-h-screen flex justify-center items-center p-4 gap-4">
-        <div className="bg-zinc-800 min-h-screen rounded-md w-1/4 flex flex-col items-center " data-aos="fade-left" data-aos-duration="500" data-aos-delay="500">
+      <div className="font-outfit bg-zinc-900 min-h-screen flex justify-center items-center p-8 gap-4">
+        <div className="bg-zinc-800 min-h-screen rounded-xl w-1/4 flex flex-col items-center " data-aos="fade-left" data-aos-duration="500" data-aos-delay="500">
           <div className="bg-zinc-700 rounded-2xl  w-max h-max mt-10 p-6">
             <img src="src/assets/profile.png" className=" h-40 w-40 animate-pulse" />
           </div>
           <br />
-          <div className="w-full text-center  text-white text-2xl " data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="800">
+          <div className="w-full text-center  text-white text-2xl tracking-wide " data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="800">
             John Patrick Pahunang
           </div>
           <br />
           <div className="w-3/4 text-center " data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="850">
-            <span className="text-white text-md bg-zinc-700 p-2 px-4 rounded-lg"> Software Engineer</span>
+            <span className="text-white text-md bg-zinc-700 p-2 px-4 rounded-lg tracking-wide"> Software Engineer</span>
           </div>
           <div className="border-b border-zinc-500 w-5/6">
             <svg height="50" width="300" xmlns="http://www.w3.org/2000/svg">
@@ -83,21 +83,37 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className="bg-zinc-800 min-h-screen w-3/4 rounded-md flex flex-col " data-aos="fade-right" data-aos-duration="500" data-aos-delay="600">
+        <div className="bg-zinc-800 min-h-screen w-3/4 rounded-xl flex flex-col " data-aos="fade-right" data-aos-duration="1000" data-aos-delay="600">
           <div className="flex justify-between items-center  w-full h-1/6 font-medium ">
-            <div className=" pl-10 text-white tracking-widest h-full flex items-end justify-end text-2xl">{route}</div>
-            <div className="flex bg-zinc-700 px-10 py-4 text-white rounded-bl-xl rounded-tr-md w-max gap-10">
+            <div
+              className=" pl-10 text-white tracking-widest h-full flex items-end justify-end text-2xl"
+              data-aos="fade-left"
+              data-aos-duration="500"
+              data-aos-easing="ease-in-out-back"
+              data-aos-delay="900"
+            >
+              {route}
+            </div>
+            <div className="flex bg-zinc-700 px-10 py-4 text-white rounded-bl-xl rounded-tr-xl w-max gap-10 " data-aos="fade" data-aos-duration="1000" data-aos-delay="1000">
               <Link to="/" onClick={() => handleRoute("About")}>
-                <div className="cursor-pointer px-4 py-2 border-b-2 border-zinc-700 hover:border-b-2 hover:border-white transition duration-300 ">About</div>
+                <div className={`cursor-pointer  px-4 py-2 border-b-2 hover:border-b-2 border-zinc-700 hover:border-white transition duration-300 ${route === "About" ? "border-white " : ""}`}>
+                  About
+                </div>
               </Link>
               <Link to="/resume" onClick={() => handleRoute("Resume")}>
-                <div className="cursor-pointer px-4 py-2 border-b-2 border-zinc-700 hover:border-b-2 hover:border-white transition duration-300">Resume</div>
+                <div className={`cursor-pointer  px-4 py-2 border-b-2 hover:border-b-2 border-zinc-700 hover:border-white transition duration-300 ${route === "Resume" ? "border-white " : ""}`}>
+                  Resume
+                </div>
               </Link>
               <Link to="/portfolio" onClick={() => handleRoute("Portfolio")}>
-                <div className="cursor-pointer px-4 py-2 border-b-2 border-zinc-700 hover:border-b-2 hover:border-white transition duration-300">Portfolio</div>
+                <div className={`cursor-pointer  px-4 py-2 border-b-2 hover:border-b-2 border-zinc-700 hover:border-white transition duration-300 ${route === "Portfolio" ? "border-white " : ""}`}>
+                  Portfolio
+                </div>
               </Link>
               <Link to="/contact" onClick={() => handleRoute("Contact")}>
-                <div className="cursor-pointer px-4 py-2 border-b-2 border-zinc-700 hover:border-b-2 hover:border-white transition duration-300 ">Contact</div>
+                <div className={`cursor-pointer  px-4 py-2 border-b-2 hover:border-b-2 border-zinc-700 hover:border-white transition duration-300 ${route === "Contact" ? "border-white " : ""}`}>
+                  Contact
+                </div>
               </Link>
             </div>
           </div>
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,5 @@
-
-
 const About = () => {
-  return <div className="h-full">MAO NI AKONG ABOUT</div>;
+  return <div className="h-full relative inline-block">MAO NI AKONG ABOUT</div>;
 };
 
 export default About;
