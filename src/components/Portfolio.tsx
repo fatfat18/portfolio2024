d93eaa8c5f9d4312895c1ba60e43efa488d93ef8
John Patrick Pahunang <patrickpahunang@Patricks-MacBook-Air.local>
2024-07-25 08:58:25 +0800

    REAL DATA TO BE INPUTTED, AND PORTFOLIO IMAGES TO BE INSERTED

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,61 +5,61 @@ const Portfolio = () => {
         <div className="w-full flex  flex-col xl:flex-row items-center justify-start  gap-6">
           <div className="xl:w-1/3 w-full ">
             {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
           </div>
           <div className="xl:w-1/3 w-full ">
             {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
           </div>
           <div className="xl:w-1/3 w-full ">
             {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
-          </div>
-        </div>
-        <div className="w-full flex flex-col xl:flex-row  items-center justify-start  gap-6">
-          <div className="xl:w-1/3 w-full ">
-            {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
-          </div>
-          <div className="xl:w-1/3 w-full ">
-            {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
-          </div>
-          <div className="xl:w-1/3 w-full ">
-            {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
           </div>
         </div>
         <div className="w-full flex flex-col xl:flex-row  items-center justify-start  gap-6">
           <div className="xl:w-1/3 w-full ">
             {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
           </div>
           <div className="xl:w-1/3 w-full ">
             {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
           </div>
           <div className="xl:w-1/3 w-full ">
             {" "}
-            <div className="h-60 w-full  rounded-xl bg-zinc-600 mb-2"></div>
-            <div className="font-semibold text-xl">Name of Project</div>
-            <div className=" text-zinc-500">Type of Project</div>
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
+          </div>
+        </div>
+        <div className="w-full flex flex-col xl:flex-row  items-center justify-start  gap-6">
+          <div className="xl:w-1/3 w-full ">
+            {" "}
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
+          </div>
+          <div className="xl:w-1/3 w-full ">
+            {" "}
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
+          </div>
+          <div className="xl:w-1/3 w-full ">
+            {" "}
+            <div className="xl:h-60 h-48 w-full  rounded-xl bg-zinc-600 mb-2"></div>
+            <div className="font-semibold xl:text-xl text-base">Name of Project</div>
+            <div className=" text-zinc-500 xl:text-base text-sm">Type of Project</div>
           </div>
         </div>
       </div>
