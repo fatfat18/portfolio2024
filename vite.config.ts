a0284e982cf59b0ac746fbf7762928fa8048d6d4
John Patrick Pahunang <patrickpahunang@Patricks-MacBook-Air.local>
2024-07-26 08:52:03 +0800

    tested

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "192.168.100.200", // Set the local IP address
-    port: 5173, // You can set the port number you prefer
-  },
+  // server: {
+  //   host: "192.168.100.200", // Set the local IP address
+  //   port: 5173, // You can set the port number you prefer
+  // },
 });
